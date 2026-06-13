069e16db4e3416007950238e1230856611d663e3
aratipalaspagar <arati.innovius@gmail.com>
2026-06-13 18:32:19 +0530

    Add files via upload

---
+++ Aarti_Portfolio_Final/js/script.js
@@ -0,0 +1,38 @@
+const words=[
+"Web Developer",
+"WordPress Developer",
+"Elementor Expert",
+"WooCommerce Specialist"
+];
+
+let wordIndex=0;
+let charIndex=0;
+
+const typing=document.getElementById("typing");
+
+function type(){
+if(charIndex<words[wordIndex].length){
+typing.textContent+=words[wordIndex].charAt(charIndex);
+charIndex++;
+setTimeout(type,100);
+}else{
+setTimeout(erase,1500);
+}
+}
+
+function erase(){
+if(charIndex>0){
+typing.textContent=
+words[wordIndex].substring(0,charIndex-1);
+charIndex--;
+setTimeout(erase,50);
+}else{
+wordIndex++;
+if(wordIndex>=words.length){
+wordIndex=0;
+}
+setTimeout(type,300);
+}
+}
+
+type();
